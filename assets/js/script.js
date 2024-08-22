b890790965e109d20d2d67d7f8a56eac01c91e2e
Paulo Nadales <p.nadales.j@gmail.com>
2024-08-22 01:52:51 -0400

    Nuevos estilos, animaciones, hover, modal de exito en formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
 const contactForm = document.getElementById("contact-form");
+const submitBtn = document.getElementById("submit-btn");
+
+submitBtn.addEventListener("click", function (e) {
+  let email = document.getElementById("email");
+  console.log(email.checkValidity());
+  let emailError = document.getElementById("email-error");
+  if (email.checkValidity()) {
+    emailError.classList.add("d-none");
+  } else {
+    emailError.classList.remove("d-none");
+  }
+});
 
 contactForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("enviar btn");
+  let loading = document.getElementById("loading");
+  loading.classList.remove("d-none");
   const form = e.target;
   const data = new FormData(form);
 
@@ -14,9 +27,13 @@ contactForm.addEventListener("submit", function (e) {
     },
   })
     .then((response) => {
+      loading.classList.add("d-none");
       if (response.ok) {
         form.reset();
         //agregat modal
+
+        let modal = new bootstrap.Modal(document.getElementById("Form-Modal"));
+        modal.show();
       } else {
         alert("Problema con response");
       }
